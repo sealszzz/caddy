991237d1a595b16c8f74d68faf721063550d008e
sealszzz <55393680+sealszzz@users.noreply.github.com>
2026-03-22 07:32:31 +0800

    Create main.go

---
+++ cmd/caddy-l4/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+
+	_ "github.com/caddyserver/caddy/v2/modules/logging"
+	_ "github.com/mholt/caddy-l4"
+)
+
+func main() {
+	caddycmd.Main()
+}
